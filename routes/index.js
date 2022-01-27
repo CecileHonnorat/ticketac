593221640de46cd6d87f85ab5321716208486557
Cecile <cecile.honnorat@gmail.com>
2022-01-27 10:43:25 +0100

    journey mongoose

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var journeyModel = require('../models/journey')
 const mongoose = require('mongoose');
 
 // useNewUrlParser ;)
@@ -12,16 +12,6 @@ var options = {
 
 // --------------------- BDD -----------------------------------------------------
 
-
-var journeySchema = mongoose.Schema({
-  departure: String,
-  arrival: String,
-  date: Date,
-  departureTime: String,
-  price: Number,
-});
-
-var journeyModel = mongoose.model('journey', journeySchema);
 
 var city = ["Paris","Marseille","Nantes","Lyon","Rennes","Melun","Bordeaux","Lille"]
 var date = ["2018-11-20","2018-11-21","2018-11-22","2018-11-23","2018-11-24"]
